01d11f2effab3eb3286c3e94a616791d989bdac0
mbonesi <mbonesi@gmail.com>
2023-11-19 00:16:28 +0100

    minimize time in mutex-protected region

--- main.cpp
+++ main.cpp
@@ -21,16 +21,19 @@ struct Queue
     std::condition_variable cv;
 } acquisitions;
 
-int producerThread(const int howMany)
+int producerThread(const int howMany, const int production_factor)
 {
     for (int i = 0; i < howMany; ++i)
     {
-        int instance = randomize(5);
-        std::cout << "producer at loop " << i << " producing: " << instance << std::endl;
+        int image;
         std::this_thread::sleep_for(std::chrono::milliseconds(15));
-        std::lock_guard<std::mutex> lock(acquisitions.m);
-        acquisitions.images.emplace(instance);
-        acquisitions.cv.notify_one();
+        
+        {   // limit time presence inside the mutex region
+            std::lock_guard<std::mutex> lock(acquisitions.m);
+            image = acquisitions.images.emplace(randomize(production_factor));
+            acquisitions.cv.notify_one();
+        }
+        std::cout << "producer at loop " << i << " producing value: " << image << std::endl;
     }
     return howMany;
 }
@@ -40,35 +43,42 @@ int consumerThread(const int howMany)
     int tot_consumed = 0;
     for (int i = 0; i < howMany; ++i)
     {
-        int consumed;
+        int image;
 
         {   // limit time presence inside the mutex region
             std::unique_lock lock(acquisitions.m);
             acquisitions.cv.wait(lock, []{ return(acquisitions.images.size() > 0); });
-            consumed = acquisitions.images.front();
+            image = acquisitions.images.front();
             acquisitions.images.pop();
         }
 
-        if(consumed != 0)
+        if(image != 0)
         {
             tot_consumed++;
-            std::cout << "consumer at loop " << i << " consuming: " << consumed << std::endl;
+            std::cout << "consumer at loop " << i << " consuming value: " << image << std::endl;
             std::this_thread::sleep_for(std::chrono::milliseconds(randomize(40)));
         }
         else
         {
-            std::cout << "consumer at loop " << i << " skipping consuming of value: " << consumed << std::endl;
+            std::cout << "consumer at loop " << i << " skipping consuming of value: " << image << std::endl;
         }
     }
     return tot_consumed;
 }
 
-
-int main()
+int main(int argc, char* argv[])
 {
+    const int FACTOR = 10;
+    int TEST_SIZE = 100;
+    
+    if(argc > 1)
+    {
+        TEST_SIZE = std::stoi(argv[1]);
+    }
+
     std::srand(std::time(nullptr));
-    auto prod = std::async(std::launch::async, producerThread, 100);
-    auto cons = std::async(std::launch::async, consumerThread, 100);
+    auto prod = std::async(std::launch::async, producerThread, TEST_SIZE, FACTOR);
+    auto cons = std::async(std::launch::async, consumerThread, TEST_SIZE);
     int created = prod.get();
     int consumed = cons.get();
     std::cout << std::endl << "SUMMARY:" << std::endl << 
